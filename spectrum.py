Fixed rotation + a bit of housekeeping

spectrum.py
@@ -10,34 +10,12 @@ DEFAULT_CHUNK_SIZE: int = 1024
 C: float = 299792.458 #km/s
 
 apply_vrad = lambda x, vrad: x*(vrad/C + 1)
+# Docelowo: vrad w log
+apply_vrad_log = lambda x, vrad: x+jnp.log10(vrad/C + 1)
 v_apply_vrad = jax.jit(jax.vmap(apply_vrad, in_axes=(None, 0)))
-atmosphere_flux = jax.jit(jax.vmap(flux, in_axes=(None, 0, None)))
+v_apply_vrad_log = jax.jit(jax.vmap(apply_vrad_log, in_axes=(None, 0)))
 
-
-@jax.jit
-def interp(x, xp, fp):
-    i = jnp.clip(jnp.searchsorted(xp, x, side='right'), 1, xp.shape[0] - 1)
-
-    fp_i0 = fp[i]
-    fp_i1 = fp[i-1]
-    xp_i0 = xp[i]
-    xp_i1 = xp[i-1]
-    
-    df = fp_i0 - fp_i1
-    dx = xp_i0 - xp_i1
-    delta = x - xp_i1
-
-    f = jnp.where((dx == 0), 
-                 fp[i], 
-                 fp[i-1] + (delta / dx) * df
-                )
-
-    f = jnp.where(x < xp[:1], fp[:1], f)
-    f = jnp.where(x > xp[-1:], fp[-1:], f)
-    return f
-
-v_interp = jax.jit(jax.vmap(interp, in_axes=(None, 0, 0)))
-
+atmosphere_flux = jax.jit(jax.vmap(flux, in_axes=(0, 0, None)))
 
 def spectrum_flash_sum(log_wavelengths,
                        areas,
@@ -52,55 +30,55 @@ def spectrum_flash_sum(log_wavelengths,
     # Just the 1D case for now
     n_areas, n_samples = areas.shape
     mus_flattened = mus.reshape(areas.shape)
+    vrads_flattened = vrads.reshape(areas.shape)
     points = log_wavelengths.shape[0]
 
     @partial(jax.checkpoint, prevent_cse=False)
     def chunk_scanner(carries, _):
-        chunk_idx, atmo_sum, areas_sum = carries
+        chunk_idx, atmo_sum, chunk_sum = carries
         k_chunk_sizes = min(chunk_size, n_areas)
 
+        # (CHUNK_SIZE, 1)
         a_chunk = lax.dynamic_slice(areas,
                                     (chunk_idx, 0),
                                     (k_chunk_sizes, n_samples))
+        
+        # (CHUNK_SIZE, 1)
         m_chunk = lax.dynamic_slice(mus_flattened,
                                     (chunk_idx, 0),
                                     (k_chunk_sizes, n_samples))
-        vrad_chunk = lax.dynamic_slice(vrads,
+        
+        # (CHUNK_SIZE, 1)
+        vrad_chunk = lax.dynamic_slice(vrads_flattened,
                                         (chunk_idx, 0),
                                         (k_chunk_sizes, n_samples))
-
+        
+        # Shape: (CHUNK_SIZE, n_wavelengths)
+        shifted_log_wavelengths = v_apply_vrad_log(log_wavelengths, vrad_chunk)
         
         # atmosphere_mul is the spectrum simulated for the corresponding wavelengths and optionally given parameters of mu, logg, and T.
         # It is then multiplied by the observed area to scale the contributions of spectra chunks
+        
         # Shape: (n_vertices, 2, n_wavelengths)
+        # Areas should be rescaled by mus
+        
         # 2 corresponds to the two components: continuum and full spectrum with lines
         atmosphere_mul = jnp.multiply(
-            a_chunk.reshape((-1, 1, 1)),
-            atmosphere_flux(log_wavelengths,
+            a_chunk.reshape((-1, 1, 1)), # Czemy nie 2D? Broadcastowanie?
+            atmosphere_flux(shifted_log_wavelengths, # (n,)
                             m_chunk,
                             parameters))
-
-        # (observed) radial velocity correction for both continuum and spectrum correction
-        # Shape: (n_vertices, 2, n_wavelengths)
-        vrad_atmosphere = jax.vmap(
-            lambda a: v_interp(
-                log_wavelengths,
-                v_apply_vrad(
-                    log_wavelengths,
-                    vrad_chunk),
-                a), in_axes=(1,))(atmosphere_mul)
         
-        # Sum the atmosphere contributions and normalize by all areas' sum
-        new_atmo_sum = atmo_sum + jnp.sum(vrad_atmosphere, axis=1)
-        new_areas_sum = areas_sum + jnp.sum(a_chunk, axis=0)
+        new_atmo_sum = atmo_sum + jnp.sum(atmosphere_mul, axis=0)#/jnp.sum(a_chunk, axis=0)
+        new_chunk_sum = chunk_sum + jnp.sum(a_chunk, axis=0)
         
-        return (chunk_idx + k_chunk_sizes, new_atmo_sum, new_areas_sum), None
+        return (chunk_idx + k_chunk_sizes, new_atmo_sum, new_chunk_sum), None
 
     # Return (2, n_vertices) for continuum and spectrum with lines
-    (_, out, areas_sum), lse = lax.scan(
+    (_, out, areas), lse = lax.scan(
         chunk_scanner,
         init=(0, jnp.zeros((2, log_wavelengths.shape[-1])), jnp.zeros(1,)),
         xs=None,
         length=math.ceil(n_areas/chunk_size))
-    return out/areas_sum
+    return out/areas
 