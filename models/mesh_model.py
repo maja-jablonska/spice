50ec85cc05c0041e7c43511b68de0e0af53e1aa0
Maja Jablonska <majajjablonska@gmail.com>
2023-05-06 16:55:35 +0200

    Add the MeshModel class

---
+++ models/mesh_model.py
@@ -0,0 +1,180 @@
+from abc import ABCMeta, abstractmethod, abstractproperty
+from dataclasses import dataclass
+import jax.numpy as jnp
+from .mesh_generation import icosphere, calculate_rotation
+from overrides import overrides
+
+
+class StarModel(metaclass=ABCMeta):
+    def __init__(self, radius: float, phases: jnp.array, timestamps: jnp.array):
+        self.__radius = radius
+        self.__phases = phases
+        self.__timestamps = timestamps
+
+    @property
+    def radius(self):
+        return self.__radius
+
+    @property
+    def phases(self) -> jnp.ndarray:
+        return self.__phases
+    
+    @property
+    def timestamps(self) -> jnp.ndarray:
+        return self.__timestamps
+    
+    @abstractproperty
+    def vertices(self) -> jnp.ndarray:
+        raise NotImplementedError
+    
+    @vertices.setter
+    def vertices(self, vertices: jnp.ndarray):
+        self.__set_vertices(vertices)
+    
+    @abstractmethod
+    def __set_vertices(self, new_vertices):
+        raise NotImplementedError
+    
+    @abstractproperty
+    def faces(self) -> jnp.ndarray:
+        raise NotImplementedError
+    
+    @faces.setter
+    def faces(self, faces: jnp.ndarray):
+        self.__set_faces(faces)
+    
+    @abstractmethod
+    def __set_faces(self, new_faces):
+        raise NotImplementedError
+    
+    @abstractproperty
+    def areas(self) -> jnp.ndarray:
+        raise NotImplementedError
+    
+    @abstractproperty
+    def centers(self) -> jnp.ndarray:
+        raise NotImplementedError
+    
+    @centers.setter
+    def centers(self, centers: jnp.ndarray):
+        self.__set_centers(centers)
+
+    @abstractproperty
+    def rotation_velocity(self) -> float:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def __set_centers(self, new_centers: jnp.ndarray):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def apply_rotation(self, omega: float, rotation_axis: jnp.ndarray) -> jnp.array:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def apply_pulsations(pulsations: jnp.array) -> jnp.array:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def get_mus(self, los_vector: jnp.ndarray) -> jnp.ndarray:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def get_velocities_vectors(self) -> jnp.ndarray:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def get_los_velocities(self, los_vector: jnp.ndarray) -> jnp.ndarray:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def get_observed_los_velocities(self, los_vector: jnp.ndarray) -> jnp.ndarray:
+        raise NotImplementedError
+    
+
+class MeshModel(StarModel):
+    def __init__(self, n_vertices: int, radius: float, phases: jnp.array, timestamps: jnp.array):
+        super().__init__(radius, phases, timestamps)
+        
+        verts, faces, areas, centers, _ = icosphere(n_vertices)
+        self.__verts = jnp.repeat(verts[:, jnp.newaxis], self.__phases.shape[0], axis=0)
+        self.__faces = jnp.repeat(faces[:, jnp.newaxis], self.__phases.shape[0], axis=0)
+        self.__areas = jnp.repeat(areas[:, jnp.newaxis], self.__phases.shape[0], axis=0)
+        self.__centers = jnp.repeat(centers[:, jnp.newaxis], self.__phases.shape[0], axis=0)
+
+        self.__radii = jnp.zeros_like(self.__centers)
+        
+        self.__omega = 0.0
+        self.__rotation_velocity = 0.0
+        self.__rotation_axis = jnp.array([0., 1., 0.])
+        self.__velocities = jnp.zeros_like(self.__centers)
+    
+    @property
+    def vertices(self) -> jnp.ndarray:
+        return self.__verts
+    
+    @vertices.setter
+    def vertices(self, vertices: jnp.ndarray):
+        self.__verts = vertices
+    
+    @property
+    def faces(self) -> jnp.ndarray:
+        return self.__faces
+    
+    @faces.setter
+    def faces(self, faces: jnp.ndarray):
+        self.__faces = faces
+    
+    @property
+    def areas(self) -> jnp.ndarray:
+        return self.__areas
+    
+    @areas.setter
+    def areas(self, areas: jnp.ndarray):
+        self.__areas = areas
+    
+    @property
+    def centers(self) -> jnp.ndarray:
+        return self.__centers
+    
+    @centers.setter
+    def centers(self, centers: jnp.ndarray):
+        self.__centers = centers
+    
+    @property
+    def rotation_velocity(self) -> float:
+        return self.__rotation_velocity
+    
+    @overrides
+    def get_mus(self, los_vector: jnp.ndarray) -> jnp.ndarray:
+        return -1.*jnp.dot(self.centers/
+                           (jnp.linalg.norm(self.centers, axis=2, keepdims=True)+1e-10),
+                           los_vector)
+
+    @overrides
+    def apply_rotation(self, omega: float,
+                       rotation_axis: jnp.ndarray = jnp.array([0., 1., 0.])) -> jnp.array:
+        self.__omega = omega
+        self.__rotation_velocity = omega*self.radius
+
+        # TODO: change to inclination
+        self.__rotation_axis = rotation_axis
+        self.centers, self.__velocities, self.__radii = calculate_rotation(self.__omega,
+                                                                           self.__rotation_axis,
+                                                                           self.centers,
+                                                                           self.timestamps)
+        return jnp.vstack([self.centers, self.__velocities])
+    
+    @overrides
+    def get_los_velocities(self,
+                           los_vector: jnp.ndarray = jnp.array([1., 0., 0.])) -> jnp.ndarray:
+        los_vels = jnp.dot(self.__velocities/(
+            jnp.linalg.norm(self.__velocities, axis=2, keepdims=True)+1e-10
+            ), los_vector)
+        return los_vels*self.__radii
+    
+    @overrides
+    def get_observed_los_velocities(self, los_vector: jnp.ndarray) -> jnp.ndarray:
+        mus = self.get_mus(los_vector)
+        mu_mask = mus>0
+        return self.get_los_velocities(los_vector)[mu_mask]
