1476f6f92faa515f882c6d68c7e8d583771f9e7c
Maja Jabłońska <majajjablonska@gmail.com>
2023-05-06 16:14:39 +0000

    Add MeshModel

--- models/mesh_model.py
+++ models/mesh_model.py
@@ -1,7 +1,7 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
 from dataclasses import dataclass
 import jax.numpy as jnp
-from .mesh_generation import icosphere, calculate_rotation
+from .mesh_generation import icosphere, calculate_rotation, apply_spherical_harm_pulsation
 from overrides import overrides
 
 
@@ -27,24 +27,8 @@ class StarModel(metaclass=ABCMeta):
     def vertices(self) -> jnp.ndarray:
         raise NotImplementedError
     
-    @vertices.setter
-    def vertices(self, vertices: jnp.ndarray):
-        self.__set_vertices(vertices)
-    
-    @abstractmethod
-    def __set_vertices(self, new_vertices):
-        raise NotImplementedError
-    
     @abstractproperty
     def faces(self) -> jnp.ndarray:
-        raise NotImplementedError
-    
-    @faces.setter
-    def faces(self, faces: jnp.ndarray):
-        self.__set_faces(faces)
-    
-    @abstractmethod
-    def __set_faces(self, new_faces):
         raise NotImplementedError
     
     @abstractproperty
@@ -54,17 +38,9 @@ class StarModel(metaclass=ABCMeta):
     @abstractproperty
     def centers(self) -> jnp.ndarray:
         raise NotImplementedError
-    
-    @centers.setter
-    def centers(self, centers: jnp.ndarray):
-        self.__set_centers(centers)
 
     @abstractproperty
     def rotation_velocity(self) -> float:
-        raise NotImplementedError
-    
-    @abstractmethod
-    def __set_centers(self, new_centers: jnp.ndarray):
         raise NotImplementedError
     
     @abstractmethod
@@ -77,10 +53,6 @@ class StarModel(metaclass=ABCMeta):
     
     @abstractmethod
     def get_mus(self, los_vector: jnp.ndarray) -> jnp.ndarray:
-        raise NotImplementedError
-    
-    @abstractmethod
-    def get_velocities_vectors(self) -> jnp.ndarray:
         raise NotImplementedError
     
     @abstractmethod
@@ -97,12 +69,12 @@ class MeshModel(StarModel):
         super().__init__(radius, phases, timestamps)
         
         verts, faces, areas, centers, _ = icosphere(n_vertices)
-        self.__verts = jnp.repeat(verts[:, jnp.newaxis], self.__phases.shape[0], axis=0)
-        self.__faces = jnp.repeat(faces[:, jnp.newaxis], self.__phases.shape[0], axis=0)
-        self.__areas = jnp.repeat(areas[:, jnp.newaxis], self.__phases.shape[0], axis=0)
-        self.__centers = jnp.repeat(centers[:, jnp.newaxis], self.__phases.shape[0], axis=0)
+        self.__verts = jnp.repeat(verts[jnp.newaxis, :, :], self.phases.shape[0], axis=0)
+        self.__faces = jnp.repeat(faces[jnp.newaxis, :, :], self.phases.shape[0], axis=0)
+        self.__areas = jnp.repeat(areas[jnp.newaxis, :], self.phases.shape[0], axis=0)
+        self.__centers = jnp.repeat(centers[jnp.newaxis, :, :], self.phases.shape[0], axis=0)
 
-        self.__radii = jnp.zeros_like(self.__centers)
+        self.__radii = jnp.zeros_like(self.__areas)
         
         self.__omega = 0.0
         self.__rotation_velocity = 0.0
@@ -112,10 +84,6 @@ class MeshModel(StarModel):
     @property
     def vertices(self) -> jnp.ndarray:
         return self.__verts
-    
-    @vertices.setter
-    def vertices(self, vertices: jnp.ndarray):
-        self.__verts = vertices
     
     @property
     def faces(self) -> jnp.ndarray:
@@ -140,6 +108,10 @@ class MeshModel(StarModel):
     @centers.setter
     def centers(self, centers: jnp.ndarray):
         self.__centers = centers
+        
+    @property
+    def rotation_axis(self) -> jnp.ndarray:
+        return self.__rotation_axis
     
     @property
     def rotation_velocity(self) -> float:
@@ -150,20 +122,25 @@ class MeshModel(StarModel):
         return -1.*jnp.dot(self.centers/
                            (jnp.linalg.norm(self.centers, axis=2, keepdims=True)+1e-10),
                            los_vector)
+    
+    @overrides
+    def apply_pulsations(pulsations: jnp.array) -> jnp.array:
+        return self.__centers
 
     @overrides
     def apply_rotation(self, omega: float,
                        rotation_axis: jnp.ndarray = jnp.array([0., 1., 0.])) -> jnp.array:
         self.__omega = omega
         self.__rotation_velocity = omega*self.radius
+        rotation_axis = rotation_axis/jnp.linalg.norm(rotation_axis)
 
         # TODO: change to inclination
         self.__rotation_axis = rotation_axis
-        self.centers, self.__velocities, self.__radii = calculate_rotation(self.__omega,
-                                                                           self.__rotation_axis,
-                                                                           self.centers,
-                                                                           self.timestamps)
-        return jnp.vstack([self.centers, self.__velocities])
+        self.__centers, self.__velocities, self.__radii = calculate_rotation(self.__omega,
+                                                                             self.__rotation_axis,
+                                                                             self.centers,
+                                                                             self.timestamps)
+        return jnp.vstack([self.__centers, self.__velocities])
     
     @overrides
     def get_los_velocities(self,
@@ -171,7 +148,7 @@ class MeshModel(StarModel):
         los_vels = jnp.dot(self.__velocities/(
             jnp.linalg.norm(self.__velocities, axis=2, keepdims=True)+1e-10
             ), los_vector)
-        return los_vels*self.__radii
+        return self.__rotation_velocity*los_vels*self.__radii
     
     @overrides
     def get_observed_los_velocities(self, los_vector: jnp.ndarray) -> jnp.ndarray:
