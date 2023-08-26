d31fecef97ff5e461cf0f16de0fc6ab06b502e7d
Maja Jablonska <majajjablonska@gmail.com>
2023-08-26 23:37:16 +0200

    IN PROGRESS passband filters

--- spectrum/__init__.py
+++ spectrum/__init__.py
@@ -1,3 +1,3 @@
-from .spectrum import simulate_spectrum, luminosity, absolute_bol_luminosity
+from .spectrum import simulate_spectrum, luminosity, passband_luminosity, filter_responses, absolute_bol_luminosity
 from .planck_law import blackbody_intensity
 from .utils import ERG_S_TO_W, SPHERE_STERADIAN, ZERO_POINT_LUM_W
