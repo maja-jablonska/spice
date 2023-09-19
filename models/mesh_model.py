9a1653412e0d99bd6f480f24b7305de71e2dc541
Maja Jablonska <majajjablonska@gmail.com>
2023-09-19 15:29:44 +1000

    Add los_velocities to the mesh model

--- models/mesh_model.py
+++ models/mesh_model.py
@@ -1,4 +1,5 @@
 from typing import Union
+from jax import jit
 from jax.typing import ArrayLike
 import jax.numpy as jnp
 
@@ -15,26 +16,26 @@ DEFAULT_LOS_VECTOR: jnp.ndarray = jnp.array([0., 1., 0.]) # from the Y direction
 DEFAULT_ROTATION_AXIS = jnp.ndarray = jnp.array([0., 0., 1.]) # from the Y direction
 
 NO_ROTATION_MATRIX = jnp.zeros((3, 3))
-MAX_PULSATION_MODES = 20
-NO_PULSATION_ARRAYS = jnp.zeros(MAX_PULSATION_MODES)
 
 class MeshModel(NamedTuple):
     # Stellar properties
+    center: ArrayLike
     radius: float
     mass: float
     abs_luminosity: float
     log_g: ArrayLike
 
     # Mesh properties
-    vertices: ArrayLike
+    # vertices and centers in the reference frame of centered on the center vector
+    d_vertices: ArrayLike
     faces: ArrayLike
-    centers: ArrayLike
+    d_centers: ArrayLike
     areas: ArrayLike
 
     parameters: ArrayLike
 
-    # Motion properties
-    velocities: ArrayLike
+    # Motion properties per-triangle
+    rotation_velocities: ArrayLike
     
     # Rotation
     rotation_axis: ArrayLike
@@ -43,17 +44,32 @@ class MeshModel(NamedTuple):
     axis_radii: ArrayLike
     rotation_velocity: ArrayLike
 
-    # Pulsation
-    pulsation_ms: ArrayLike
-    pulsation_ns: ArrayLike
-    t_zeros: ArrayLike
-    pulsation_periods: ArrayLike
-    amplitudes: ArrayLike
+    orbital_velocity: float
 
     # Mesh LOS properties
     los_vector: ArrayLike
+    los_vertices: ArrayLike
     mus: ArrayLike
     los_velocities: ArrayLike
+
+    @property
+    def vertices(self) -> ArrayLike:
+        if len(self.d_vertices.shape)==2:
+            return self.d_vertices + self.center
+        else:
+            return self.d_vertices + self.center.reshape((self.d_vertices.shape[0], *([1]*(len(self.d_vertices.shape)-2)), self.d_vertices.shape[-1]))
+    
+    @property
+    def centers(self) -> ArrayLike:
+        if len(self.d_centers.shape) == 2:
+            return self.d_centers + self.center
+        else:
+            return self.d_centers + self.center.reshape((self.d_centers.shape[0], *([1]*(len(self.d_centers.shape)-2)), self.d_centers.shape[-1]))
+
+    @property
+    def velocities(self) -> jnp.float64:
+        return self.rotation_velocities + self.orbital_velocity
+
 
     @abstractmethod
     def pulsation_modes(self) -> int:
@@ -87,13 +103,16 @@ class IcosphereModel(MeshModel):
         if len(parameters.shape) == 1:
             parameters = jnp.repeat(parameters[jnp.newaxis, :], repeats = areas.shape[0], axis = 0)
 
-        return MeshModel.__new__(cls, radius, mass, abs_luminosity, log_g*jnp.ones_like(areas),
-                vertices*radius, faces, centers*radius, areas*sphere_area/jnp.sum(areas), parameters,
-                jnp.zeros_like(centers),
-                DEFAULT_ROTATION_AXIS, NO_ROTATION_MATRIX, NO_ROTATION_MATRIX, calculate_axis_radii(centers, DEFAULT_ROTATION_AXIS), 0.,
-                NO_PULSATION_ARRAYS, NO_PULSATION_ARRAYS, NO_PULSATION_ARRAYS, NO_PULSATION_ARRAYS, NO_PULSATION_ARRAYS,
-                DEFAULT_LOS_VECTOR, cast_to_los(centers, DEFAULT_LOS_VECTOR), jnp.zeros_like(areas))
-    
-    @overrides
-    def pulsation_modes(self) -> int:
-        return jnp.count_nonzero(self.amplitudes)
+        return MeshModel.__new__(cls, 0., radius, mass, abs_luminosity, log_g*jnp.ones_like(areas),
+                d_vertices=vertices*radius, faces=faces, d_centers=centers*radius, areas=areas*sphere_area/jnp.sum(areas), parameters=parameters,
+                rotation_velocities=jnp.zeros_like(centers),
+                rotation_axis=DEFAULT_ROTATION_AXIS,
+                rotation_matrix=NO_ROTATION_MATRIX,
+                rotation_matrix_prim=NO_ROTATION_MATRIX,
+                axis_radii=calculate_axis_radii(centers, DEFAULT_ROTATION_AXIS),
+                rotation_velocity=0.,
+                orbital_velocity=0.,
+                los_vector=DEFAULT_LOS_VECTOR,
+                los_vertices=cast_to_los(vertices*radius, DEFAULT_LOS_VECTOR),
+                mus=cast_to_los(centers, DEFAULT_LOS_VECTOR),
+                los_velocities=jnp.zeros_like(areas))
