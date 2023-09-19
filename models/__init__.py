9b008d07aaa4090422f104faaaaa28c21f0c8f72
Maja Jablonska <majajjablonska@gmail.com>
2023-09-19 15:29:44 +1000

    Add los_velocities to the mesh model

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .mesh_generation import icosphere
 from .mesh_model import IcosphereModel, MeshModel
 from .abundances import Abundance
-from .mesh_transform import add_pulsation, evaluate_pulsation
 from .spots import add_spot, add_spots
+from .binary import Binary
+from .orbit_utils import get_orbit_jax
