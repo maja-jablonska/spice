7542bb947fdb02895dc85076d56975b655e3f559
Maja Jablonska <majajjablonska@gmail.com>
2023-11-14 14:10:57 +1100

    Grid-based occlusiuon

--- geometry/utils.py
+++ geometry/utils.py
@@ -137,7 +137,6 @@ def polygon_area(x: ArrayLike, y: ArrayLike) -> ArrayLike:
 polygon_areas = jax.jit(jax.vmap(polygon_area, in_axes=(0, 0)))
 
 def cast_indexes(cast_vertices: ArrayLike):
-    jax.debug.print("Getting cast indexes")
     zeroth_ind = jnp.argmax(jnp.all(jnp.isclose(cast_vertices, 0), axis=1))
     # Looks silly, but it needs to be jitable
     return jax.lax.cond(zeroth_ind == 0,
@@ -148,7 +147,6 @@ def cast_indexes(cast_vertices: ArrayLike):
 
 
 def get_cast_areas(face_vertices: ArrayLike) -> ArrayLike:
-    jax.debug.print("Getting cast areas")
     cast_ind = cast_indexes(face_vertices)
     return polygon_areas(face_vertices[:, :, cast_ind[0]],
                          face_vertices[:, :, cast_ind[1]])
