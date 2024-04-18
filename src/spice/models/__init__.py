79388175ceba5fd78927035e841db38433b904a8
Maja Jablonska <majajjablonska@gmail.com>
2024-04-18 12:31:47 +1000

    Remove "abundances.py"

--- src/spice/models/__init__.py
+++ src/spice/models/__init__.py
@@ -1,6 +1,5 @@
 from .mesh_generation import icosphere
 from .mesh_model import IcosphereModel, MeshModel
-from .abundances import Abundance
 from .spots import add_spot, add_spots
 from .binary import Binary
 from .orbit_utils import get_orbit_jax
