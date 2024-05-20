ac5058eba5d651098a902e0a635f0b04e80bbfdd
Maja Jablonska <majajjablonska@gmail.com>
2024-05-20 14:56:12 +1000

    [WIP] PHOEBE-based mesh model

--- src/spice/models/__init__.py
+++ src/spice/models/__init__.py
@@ -1,5 +1,7 @@
+from .model import Model
 from .mesh_generation import icosphere
 from .mesh_model import IcosphereModel, MeshModel
 from .spots import add_spot, add_spots
 from .binary import Binary
 from .orbit_utils import get_orbit_jax
+from .phoebe_model import PhoebeModel
