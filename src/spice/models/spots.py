e02c75c5760dfb7192a252a6003e67f264d977e0
Maja Jablonska <majajjablonska@gmail.com>
2024-05-29 13:52:50 +1000

    Add spherical harmonics as spot  parameters

--- src/spice/models/spots.py
+++ src/spice/models/spots.py
@@ -31,33 +31,6 @@ def __overabundance_spot_params(theta, phi, abun, abun_bg, radius, radius_factor
 
 v_spot = jax.vmap(__overabundance_spot_params, in_axes=(None, None, None, None, None, None, 0))
 
-
-@jax.jit
-def add_spot(mesh: MeshModel,
-             theta: float,
-             phi: float,
-             param_delta: float,
-             radius: float,
-             radius_factor: float,
-             param_index: int) -> MeshModel:
-    """Add a spot to the mesh model.
-
-    Args:
-        mesh (MeshModel): mesh model
-        theta (float): theta coordinate of the spot center
-        phi (float): phi coordinate of the spot center
-        param_delta (float): difference in the parameter value between the spot and the background
-        radius (float): spot radius in radians
-        radius_factor (float): factor of the difference gradient on the spot edge
-        param_index (int): index of the parameter in the parameters array
-
-    Returns:
-        MeshModel: mesh with the spot added
-    """
-    spot_parameters = v_spot(theta, phi, param_delta, 0., radius, radius_factor, mesh.centers)
-    return mesh._replace(parameters = mesh.parameters.at[:, param_index].set(
-        mesh.parameters[:, param_index]+spot_parameters))
-    
     
 @jax.jit
 def add_spherical_harmonic_spot(mesh: MeshModel,
@@ -79,38 +52,10 @@ def add_spherical_harmonic_spot(mesh: MeshModel,
     center_polar_coords = mesh_polar_vertices(mesh.centers)
     spot_parameters = spherical_harmonic(m, n, center_polar_coords) * param_delta
     return mesh._replace(parameters = mesh.parameters.at[:, param_index].set(
-        mesh.parameters[:, param_index]+spot_parameters[:, jnp.newaxis]))
+        mesh.parameters[:, param_index]+spot_parameters.reshape((-1, 1))))
 
 
-def add_spots(mesh: MeshModel,
-              thetas: ArrayLike,
-              phis: ArrayLike,
-              param_deltas: ArrayLike,
-              radii: ArrayLike,
-              radius_factors: ArrayLike,
-              param_indices: ArrayLike) -> MeshModel:
-    """Add multiple spots to the mesh model
-
-    Args:
-        mesh (MeshModel): mesh model
-        thetas (ArrayLike): theta coordinates of the spots centers
-        phis (ArrayLike): phi coordinates of the spots centers
-        param_deltas (ArrayLike): difference in the parameter valuea between the spota and the background
-        radii (ArrayLike): spot radii in radians
-        radius_factors (ArrayLike): factors of the difference gradients on the spot edges
-        param_indices (ArrayLike): indices of the parameters in the parameters array
-
-    Returns:
-        MeshModel: mesh with the spots added
-    """
-    def scan(carry, params):
-        return add_spot(carry, *params[:-1], params[-1].astype(int)), params
-    
-    updated_mesh, _ = jax.lax.scan(scan, mesh, jnp.vstack([thetas, phis, param_deltas, radii, radius_factors, param_indices]).T)
-    return updated_mesh
-
-
-@jax.jit
+# @jax.jit
 def add_spherical_harmonic_spots(mesh: MeshModel,
                                  m: ArrayLike, n: ArrayLike,
                                  param_deltas: ArrayLike,
@@ -129,7 +74,7 @@ def add_spherical_harmonic_spots(mesh: MeshModel,
     """
     def scan(carry, params):
         return add_spherical_harmonic_spot(
-            carry, params[0].astype(int), params[1].astype(int), params[2], params[3].astype(int)
+            carry, m=params[0].astype(int), n=params[1].astype(int), param_delta=params[2], param_index=jnp.atleast_1d(params[3].astype(int))
             ), params
         
     updated_mesh, _ = jax.lax.scan(scan, mesh, jnp.vstack([m, n, param_deltas, param_indices]).T)
