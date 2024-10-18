4c322ea2fcb7f7e94636df4925adad753e2fe674
Maja Jablonska <majajjablonska@gmail.com>
2024-10-18 19:19:47 +1100

    Add tilted pulsations

--- tests/test_models/test_spots.py
+++ tests/test_models/test_spots.py
@@ -51,9 +51,9 @@ class TestSpotFunctions:
             add_spherical_harmonic_spot(mock_mesh, 1, 0, 10, 0)
 
     def test_raises_error_for_m_larger_than_n_add_spherical_harmonic_spots(self, mock_mesh, spherical_harmonic_params):
-        m_values, n_values, param_deltas, param_indices = spherical_harmonic_params.values()
+        _, _, param_deltas, param_indices = spherical_harmonic_params.values()
         with pytest.raises(ValueError):
-            add_spherical_harmonic_spots(mock_mesh, 1, 0, param_deltas, param_indices)
+            add_spherical_harmonic_spot(mock_mesh, 1, 0, param_deltas[0], param_indices[0])
 
     def test_raises_error_for_read_only_phoebe_model_add_spherical_harmonic_spots(self, spherical_harmonic_params, shared_datadir):
         phoebe_mesh = pickle.load(open(shared_datadir / "phoebe_model.pkl", "rb"))
@@ -83,3 +83,85 @@ class TestSpotFunctions:
     def test_add_spot_with_large_radius_modifies_entire_mesh(self, mock_mesh):
         result = add_spot(mock_mesh, jnp.pi / 2, 0.0, 400., 1.0, 0)
         assert not jnp.any(jnp.isclose(result.parameters[:, 0], mock_mesh.parameters[:, 0]))
+
+    def test_add_tilted_spherical_harmonic_spot(self, mock_mesh):
+        base_temp = 5700
+        spot_temp = 15000
+        tilt_axis = jnp.array([1., 0., 0.])
+        tilt_degree = 10.
+
+        modified_mesh = add_spherical_harmonic_spot(
+            mesh=mock_mesh, 
+            m_order=4, 
+            n_degree=4,
+            param_delta=spot_temp - base_temp,
+            param_index=0, 
+            tilt_axis=tilt_axis, 
+            tilt_degree=tilt_degree
+        )
+
+        assert not jnp.allclose(modified_mesh.parameters, mock_mesh.parameters)
+        
+        # Check if the tilt has been applied
+        untilted_mesh = add_spherical_harmonic_spot(
+            mesh=mock_mesh, 
+            m_order=4, 
+            n_degree=4,
+            param_delta=spot_temp - base_temp,
+            param_index=0
+        )
+        
+        assert not jnp.allclose(modified_mesh.parameters, untilted_mesh.parameters)
+
+    def test_add_tilted_spherical_harmonic_spot_with_zero_tilt(self, mock_mesh):
+        base_temp = 5700
+        spot_temp = 15000
+        tilt_axis = jnp.array([1., 0., 0.])
+        tilt_degree = 0.
+
+        modified_mesh = add_spherical_harmonic_spot(
+            mesh=mock_mesh, 
+            m_order=4, 
+            n_degree=4,
+            param_delta=spot_temp - base_temp,
+            param_index=0, 
+            tilt_axis=tilt_axis, 
+            tilt_degree=tilt_degree
+        )
+
+        untilted_mesh = add_spherical_harmonic_spot(
+            mesh=mock_mesh, 
+            m_order=4, 
+            n_degree=4,
+            param_delta=spot_temp - base_temp,
+            param_index=0
+        )
+        
+        assert jnp.allclose(modified_mesh.parameters, untilted_mesh.parameters)
+
+    def test_add_tilted_spherical_harmonic_spot_different_tilt_axes(self, mock_mesh):
+        base_temp = 5700
+        spot_temp = 15000
+        tilt_degree = 45.
+
+        modified_mesh_x = add_spherical_harmonic_spot(
+            mesh=mock_mesh, 
+            m_order=4, 
+            n_degree=4,
+            param_delta=spot_temp - base_temp,
+            param_index=0, 
+            tilt_axis=jnp.array([1., 0., 0.]), 
+            tilt_degree=tilt_degree
+        )
+
+        modified_mesh_y = add_spherical_harmonic_spot(
+            mesh=mock_mesh, 
+            m_order=4, 
+            n_degree=4,
+            param_delta=spot_temp - base_temp,
+            param_index=0, 
+            tilt_axis=jnp.array([0., 1., 0.]), 
+            tilt_degree=tilt_degree
+        )
+
+        assert not jnp.allclose(modified_mesh_x.parameters, modified_mesh_y.parameters)
--- tests/test_models/utils.py
+++ tests/test_models/utils.py
@@ -1,10 +1,5 @@
 from spice.models import IcosphereModel
 
-
-SOLAR_RAD_CM = 69570000000.0
-SOLAR_MASS_KG = 1.988409870698051e+30
-
-
 def default_icosphere():
-    return IcosphereModel.construct(100, SOLAR_RAD_CM, SOLAR_MASS_KG,
+    return IcosphereModel.construct(100, 1., 1.,
                                     [5700, 0.], ['teff', 'abun'])
