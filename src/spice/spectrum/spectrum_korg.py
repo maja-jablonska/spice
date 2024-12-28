b13bfac4991885fc59a053f904231fffbf6591a1
Maja Jablonska <majajjablonska@gmail.com>
2024-12-28 15:46:14 +0100

    Fix bug in spectrum_korg intensity

--- src/spice/spectrum/spectrum_korg.py
+++ src/spice/spectrum/spectrum_korg.py
@@ -164,4 +164,4 @@ class KorgSpectrumEmulator(SpectrumEmulator[ArrayLike]):
 
     @override
     def intensity(self, log_wavelengths: ArrayLike, mu: float, parameters: ArrayLike) -> ArrayLike:
-        return self.interpolate_spectrum(self.log10_wavelengths, self.continuum_wavelengths, self.intensities, self.continuum_intensities, jnp.hstack([parameters, jnp.atleast_1d(mu)]), log_wavelengths)
+        return interpolate_spectrum(self.log10_wavelengths, self.continuum_wavelengths, self.intensities, self.continuum_intensities, jnp.hstack([parameters, jnp.atleast_1d(mu)]), log_wavelengths)
