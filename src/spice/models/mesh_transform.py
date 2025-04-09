Minor utilities and rotation fix

src/spice/models/mesh_transform.py
@@ -218,7 +218,7 @@ def add_rotation(mesh: MeshModel,
 @jax.jit
 def _evaluate_rotation(mesh: MeshModel, t: ArrayLike) -> MeshModel:
     rotation_velocity_km_s = mesh.rotation_velocity
-    theta = (rotation_velocity_km_s * t) / 695700.0
+    theta = (rotation_velocity_km_s * t) / (mesh.radius * 695700.0)
     
     t_rotation_matrix = evaluate_rotation_matrix(mesh.rotation_matrix, theta)
     
@@ -228,7 +228,7 @@ def _evaluate_rotation(mesh: MeshModel, t: ArrayLike) -> MeshModel:
     
     rotated_centers = jnp.matmul(mesh.d_centers, t_rotation_matrix)
     
-    rotated_centers_vel = rotation_velocity_km_s * jnp.matmul(mesh.d_centers, t_rotation_matrix_prim)
+    rotated_centers_vel = rotation_velocity_km_s * jnp.matmul(mesh.d_centers/mesh.radius, t_rotation_matrix_prim)
 
     new_axis_radii = calculate_axis_radii(rotated_centers, mesh.rotation_axis)
     return mesh._replace(d_vertices=rotated_vertices,