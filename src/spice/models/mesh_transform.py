cf98f2eb3b66d1de08909dffb557ea7496c5e7b4
Maja Jablonska <majajjablonska@gmail.com>
2025-04-09 17:52:36 +1000

    Add utility functions for latitude and longitude conversions, modify mesh parameters, and update mesh plotting details. Fix parameter documentation for spot radius. Improve rotation calculations in mesh transformations.

--- src/spice/models/mesh_transform.py
+++ src/spice/models/mesh_transform.py
@@ -218,7 +218,7 @@ def add_rotation(mesh: MeshModel,
 @jax.jit
 def _evaluate_rotation(mesh: MeshModel, t: ArrayLike) -> MeshModel:
     rotation_velocity_km_s = mesh.rotation_velocity
-    theta = (rotation_velocity_km_s * t) / mesh.radius / 695700.0
+    theta = (rotation_velocity_km_s * t) / 695700.0
     
     t_rotation_matrix = evaluate_rotation_matrix(mesh.rotation_matrix, theta)
     
